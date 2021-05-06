2763ee8dfcabc853559e256e1b61ed283d3abd95
Michael Nnaji <mnnaji3@uwo.ca>
2021-05-06 10:55:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
-const companyName = "Scruber"
-let mostProfitableNeighborhood = "Chelsea"
-let companyName = "Susan Smith"
+const companyName = "Scruber";
+let mostProfitableNeighborhood = "Chelsea";
+let companyName = "Susan Smith";
